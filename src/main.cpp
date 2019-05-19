e3d2cf02286acd5c949ff0c52e9552cc591149f9
Karim Mansour <senegalo@gmail.com>
2019-05-19 14:55:28 +0200

    get requests sent from serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,55 @@
 #include <Arduino.h>
-#include "ESP8266WiFi.h"
-#include "WiFiClient.h"
+#include <WiFiClientSecureBearSSL.h>
 #include "ESP8266HTTPClient.h"
+#include "ESP8266WiFi.h"
+#include "ESP8266WiFiMulti.h"
+#include "WiFiClient.h"
 
 const char* ssid = "NinjaTurtles";
 const char* password = "FireFighter2";
 
+ESP8266WiFiMulti WiFiMulti;
+
+void getRequest(String url) {
+  WiFiClient client;
+  HTTPClient http;
+
+  if (http.begin(client, url)) {
+    Serial.print("Request Started Sending headers");
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.print("Status Code: " + httpCode);
+
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = http.getString();
+        Serial.print("Payload is: " + payload);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n",
+                    http.errorToString(httpCode).c_str());
+    }
+
+    Serial.println("Closing Http Connection");
+    http.end();
+    Serial.println("Closed Http Connection");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.print("\n\r \n\rWorking to connect");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("DHT Weather Reading Server");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  Serial.println("HTTP server started");
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(ssid, password);
 }
 
 void loop() {
-  Serial.println("Start Loop");
-  if(WiFi.status() == WL_CONNECTED){
-    HTTPClient http;
-    WiFiClient client;
-
-    // put your main code here, to run repeatedly:
-    Serial.print("Starting GET request");
-    if(http.begin(client, "http://192.168.178.29:8080")) {
-      Serial.print("Request Started Sending headers");
-      int httpCode = http.GET();
-      if(httpCode > 0) {
-        Serial.print("Status Code: " + httpCode);
-        
-        if(httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = http.getString();
-          Serial.print("Payload is: " + payload);
-        }
-      } else {
-          Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-
-      Serial.println("Closing Http Connection");
-      http.end();
-      Serial.println("Closed Http Connection");
+  if (WiFiMulti.run() == WL_CONNECTED) {
+    if (Serial.available() > 0) {
+      String url = Serial.readString();
+      Serial.print("Starting GET request for " + url);
+      getRequest(url);
     }
   } else {
-    Serial.println("Not Connected !!");
+    Serial.println("No Connected Yet !");
   }
-  Serial.println("End loop");
+  delay(1000);
 }
